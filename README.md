28938d431699d040a2d780eb59e53d0e2eb61f8c
Justin Merz <jrmerz@gmail.com>
2015-10-29 14:21:55 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # mongo-object-history
+
 Object history tracking for NodeJS/MongoDB
+
+Tracks objects by type and id.  stores
+ - user identifier
+ - object identifier
+ - timestamp
+ - object diff
+If the object is new
+ - initial object
+ - created flag
+
+## Configure
+
+```
+var history = require('mongo-object-history');
+
+var collection = mongo.collection('widgets');
+
+var config = {
+  get : {
+    // you need to define a getter for each type you want to track
+    widget : function(id, callback) {
+      collection.findOne(id, callback);
+    }
+  },
+  collection : collection
+}
+history.init(config);
+```
+
+## Use
+Then to track
+```
+var history = require('mongo-object-history');
+
+var newWidget = {
+  id : '....',
+  ....
+}
+var user = 'foobar-user';
+
+history.track('widget', newWidget, user, function(err, result){
+  // object is tracked
+  // err is either custom error string or mongodb error
+  // result is mongodb response from insert
+});
+```
---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var diff = require('deep-diff').diff;
+
+var config;
+var idParam = 'id';
+var trackMongoId = false;
+
+function init(c) {
+  c = config;
+
+  if( config.id ) {
+    idParam = config.id;
+  }
+  if( config.trackMongoId ) {
+    trackMongoId = true;
+  }
+}
+
+function track(type, object, user, callback) {
+  var id = object[idParam];
+  if( !id ) {
+    return callback('Object has no id');
+  }
+
+  config.get[type](id, function(err, currentObject){
+    if( err ) {
+      callback(err);
+    }
+
+    var history = {
+      id : id,
+      user : user,
+      datetime : new Date(),
+      type : type
+    };
+  });
+}
+
+function onObjectGet(history, newObject, currentObject, callback) {
+  if( currentObject === null ) {
+    history.created = true;
+    history.original = JSON.stringify(newObject);
+  } else {
+    if( !trackMongoId ) {
+      delete currentObject._id;
+    }
+
+    var differences = diff(currentObject, newObject);
+    if( !differences ) {
+      return callback(null, {nochange: true});
+    }
+    history.diff = JSON.stringify(differences);
+  }
+
+  config.collection.insert(history, callback);
+}
+
+
+module.exports = {
+  init : init,
+  track : track
+};
